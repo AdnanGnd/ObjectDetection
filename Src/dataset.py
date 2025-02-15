049de13ef8a3ec5505c2f1349edc391c07052aef
Adnane GUENDAOUI <adnanegnd@gmail.com>
2025-02-15 19:48:04 +0100

    Version 1

--- Src/dataset.py
+++ Src/dataset.py
@@ -4,16 +4,6 @@ from torch.utils.data import Dataset
 import torch
 from torchvision.transforms import ToTensor
 
-def yolo_to_bbox(yolo_bbox, image_width, image_height):
-    """
-    Convert YOLO format [class_id, x_center, y_center, width, height] to [x_min, y_min, x_max, y_max].
-    """
-    class_id, x_center, y_center, width, height = yolo_bbox
-    x_min = (x_center - width / 2) * image_width
-    y_min = (y_center - height / 2) * image_height
-    x_max = (x_center + width / 2) * image_width
-    y_max = (y_center + height / 2) * image_height
-    return [x_min, y_min, x_max, y_max]
 
 class CustomObjectDetectionDataset(Dataset):
     def __init__(self, image_dir, label_dir, transform=None):
